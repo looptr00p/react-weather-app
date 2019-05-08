548e69b027a61232d86358a74223fa8ae147bc54
Nicolas <nicolas.herera@hotmail.com>
2019-05-08 03:24:49 -0400

    minor changes

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,6 @@ FROM node:8-alpine
 RUN mkdir -p /usr/src/app
 COPY . /usr/src/app
 WORKDIR /usr/src/app
-RUN yarn
-EXPOSE 8000
+RUN npm i
+EXPOSE 3000
 CMD ["npm", "start"]
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -5,7 +5,7 @@ import './Home.css';
 class Home extends Component {
 
     render() {
-        let heading = "Soaring to new heights";
+        let heading = "Discover a new weather";
         let subheading = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
         return (
@@ -17,7 +17,7 @@ class Home extends Component {
                     <div className="is-two-thirds column is-paddingless">
                         <h2 className="subtitle is-4">{ subheading }</h2>
                     </div>
-                    <a className="button is-large is-primary" id="learn" href="https://github.com/looptr00p">Learn more</a>
+                    <a className="button is-large is-primary" id="learn" href="https://github.com/looptr00p">Visit GitHub</a>
                     </div>
                 </div>
             </section>
